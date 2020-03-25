e480c3ed2487151cae4da25da98d74ae5d90ee37
Stefan van Horssen <sv_horssen@hotmail.com>
2020-03-25 16:32:59 +0100

    Loginscherm input-checks, foutmeldingen en doorsturen naar welkom-scherm, zonder database-connectie

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,7 +1,10 @@
 package controller;
 
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import view.Main;
 
 public class LoginController {
 
@@ -15,19 +18,50 @@ public class LoginController {
 
         // check of user bestaat
             // input ophalen uit username-vakje
+        String usernameInput = nameTextField.getText();
 
-            // check of username-input een waarde heeft
+            // check of username-input een waarde heeft -> als leeg, geef melding
+        if(usernameInput.trim().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Deze gebruikersnaam is niet bekend, probeer het nog eens.");
+            alert.show();
+            return;
+        }
+
             // check of deze user bestaat in het systeem
                 // haal alle info van user op uit database
-                // als er geen info is opgehaald -> dan bestaat deze user niet
-                // -> geef dan foutmelding (neem contact op met technisch beheerder)
+            //TODO:
+            // gebruik userDAO om gebruikersgegevens op te halen en stop resultaat in opgehaaldeGebruikersnaam
+            String opgehaaldeGebruikersnaam = "Stefan"; // TODO nog koppelen aan DAO -> in een try-catch zetten
+            // als er geen info is opgehaald -> dan bestaat deze user niet
+            if (opgehaaldeGebruikersnaam.isEmpty()) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Deze gebruikersnaam is niet bekend, probeer het nog eens.");
+                alert.show();
+                return;
+            }
 
         // checken of wachtwoord ingevoerd is gelijk aan wachtwoord user
             // van de opgehaalde user, check of wachtwoord gelijk is aan ingevoerd wachtwoord
-            // input ophalen uit wachtwoord vakje
+                // input ophalen uit wachtwoord vakje
+        String wachtwoordInput = passwordField.getText();
                 // haal wachtwoord op, op basis van username
-                // check of deze gelijk zijn -> zo niet -> geef foutmelding
-                // als ze gelijk zijn -> ga door naar inlogschermen
+        //TODO:
+        // gebruik userDAO om gebruikersgegevens op te halen en stop resultaat in opgehaaldeWachtwoord
+        String opgehaaldeWachtwoord = "Stefan";// functie: haalWachtwoordOpObvGebruikersnaam(Gebruikersnaam)
+        // TODO nog koppelen aan DAO -> in een try-catch zetten
+
+        // check of deze gelijk zijn -> zo niet -> geef foutmelding
+        if (opgehaaldeWachtwoord.equals(wachtwoordInput)) {
+            // als ze gelijk zijn -> ga door naar inlogschermen
+            Main.getSceneManager().showWelcomeScene();
+        } else{
+            // wachtwoord is onjuist
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Het wachtwoord is niet juist, probeer het nog eens.");
+            alert.show();
+            return;
+        }
 
         // bepalen welke inlogscherm getoond moet worden
         // data ophalen uit database (rest van info)
