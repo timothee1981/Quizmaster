31c5616765f38fd6b43d471bb7b94e451b89314f
Timothee <timotheebusch@hotmail.com>
2020-04-07 17:51:10 +0200

    code verschonen Answer, createUpdateController

--- src/main/java/controller/CreateUpdateQuestionController.java
+++ src/main/java/controller/CreateUpdateQuestionController.java
@@ -1,9 +1,6 @@
 package controller;
 
 import database.mysql.*;
-import database.nosql.CouchDBaccess;
-import database.nosql.QuestionCouchDBAO;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
@@ -12,15 +9,12 @@ import javafx.scene.control.TextField;
 import model.Answer;
 import model.Question;
 import model.Quiz;
-import model.User;
 import view.Main;
 
 import java.util.ArrayList;
 
 public class CreateUpdateQuestionController {
-    private DBAccess dbAccess = new DBAccess(DBAccess.getDatabaseName(), DBAccess.getMainUser(), DBAccess.getMainUserPassword());;
-    private QuestionDAO questionDAO;
-    private AnswerDAO answerDAO;
+    private DBAccess dbAccess = new DBAccess(DBAccess.getDatabaseName(), DBAccess.getMainUser(), DBAccess.getMainUserPassword());
     private Question question;
     private ArrayList<Answer> answers;
     private String labelvul;
@@ -29,7 +23,15 @@ public class CreateUpdateQuestionController {
 
 
     @FXML
-    private  Label idQuestion, idGoodAnswer,idAnswer2 ,idAnswer3,idAnswer4;
+    private  Label idQuestion;
+    @FXML
+    private Label idGoodAnswer;
+    @FXML
+    private Label idAnswer2;
+    @FXML
+    private Label idAnswer3;
+    @FXML
+    private Label idAnswer4;
 
     @FXML
     private Label titelLabel;
@@ -54,7 +56,6 @@ public class CreateUpdateQuestionController {
 
     public void setup(Question question) {
         dbAccess.openConnection();
-        answerDAO = new AnswerDAO(dbAccess);
 
         // vul dropdown met alle quizes
         fillQuizDropdown();
@@ -70,139 +71,155 @@ public class CreateUpdateQuestionController {
             // bestaande vraaag
             labelwijzig = "Wijzig Vraag";
             titelLabel.setText(labelwijzig);
-
             // vul vraag-textveld in:
-            vraagTextField.setText(question.getQuestion());
-            int questionId = question.getQuestionId();
-            String questionIdString = String.format("%d",questionId);
-            idQuestion.setText(questionIdString);
-
+            setQuestionTextField(question);
             // vul antwoorden in:
-            goodAnswerTextField.setText(question.getAnswers().get(0).toString());
-            answer2TextField.setText(question.getAnswers().get(1).toString());
-            answer3TextField.setText(question.getAnswers().get(2).toString());
-            answer4TextField.setText(question.getAnswers().get(3).toString());
-            String goodAnswerId = String.format("%d",question.getAnswers().get(0).getAnswerId());
-            String answer2 = String.format("%d",question.getAnswers().get(1).getAnswerId());
-            String answer3 = String.format("%d",question.getAnswers().get(2).getAnswerId());
-            String answer4 = String.format("%d",question.getAnswers().get(3).getAnswerId());
-            idGoodAnswer.setText(goodAnswerId);
-            idAnswer2.setText(answer2);
-            idAnswer3.setText(answer3);
-            idAnswer4.setText(answer4);
+            setAnswersField(question.getAnswers());
+           //zet idlabel op waarde van id aantwoorden
+            setAnswersLableId(question.getAnswers());
 
 
         }
+    }
+
+    //methode om question textfield te vullen
+    private void setQuestionTextField(Question question) {
+        vraagTextField.setText(question.getQuestion());
+        int questionId = question.getQuestionId();
+        String questionIdString = String.format("%d",questionId);
+        idQuestion.setText(questionIdString);
+    }
+
+    // methoden om Antwoord field te vullen
+    private void setAnswersField(ArrayList<Answer> answers) {
+        goodAnswerTextField.setText(answers.get(0).toString());
+        answer2TextField.setText(answers.get(1).toString());
+        answer3TextField.setText(answers.get(2).toString());
+        answer4TextField.setText(answers.get(3).toString());
+    }
+
+    //methode om waarde te geven aan id antwoord label
+
+    private void setAnswersLableId(ArrayList<Answer> answers) {
+        String goodAnswerId = String.format("%d",answers.get(0).getAnswerId());
+        String answer2 = String.format("%d",answers.get(1).getAnswerId());
+        String answer3 = String.format("%d",answers.get(2).getAnswerId());
+        String answer4 = String.format("%d",answers.get(3).getAnswerId());
+        idGoodAnswer.setText(goodAnswerId);
+        idAnswer2.setText(answer2);
+        idAnswer3.setText(answer3);
+        idAnswer4.setText(answer4);
     }
 
     private void setQuizDropdownOfUser(Quiz quiz) {
         quizComboBox.setValue(quiz);
     }
 
-    public ArrayList<Answer> returnArrayAnswers(int questionId){
-        dbAccess.openConnection();
-        AnswerDAO answerDAO = new AnswerDAO(dbAccess);
-        ArrayList<Answer> answers;
-        answers = answerDAO.getAnswersByQuestionId(questionId);
-        return answers;
-    }
+
 
     public void doMenu() {
         Main.getSceneManager().showCoordinatorDashboard();
     }
 
     @FXML
-    public void doCreateUpdateQuestion(ActionEvent actionEvent) {
+    public void doCreateUpdateQuestion() {
        createQuestion();
       //  createAnswerBijQuestion(question);
-
-        // due to global datastorage - first clear question/answer etc.
-
-
         // maak database-connectie
         dbAccess.openConnection();
-
-        questionDAO = new QuestionDAO(dbAccess);
-        answerDAO = new AnswerDAO(dbAccess);
-
-
         //check als er een nieuwe vraag moet komen of als een bestaande vraag moet gewijzigd worden
         if (titelLabel.getText().equals(labelvul)) {//als titel op nieuwe is-----> nieuwe vraag
-            //check als combobox waarde heeft
-            if(question != null) {
-                if (!(quizComboBox.getValue() == null)) {
-                    //als combobox waarde creer vraag
-                    // vul quiz in die bij vraag hoort
-                    question.setQuiz(quizComboBox.getValue());
-                    // check of je vraag een waarde heeft:
-                    if (!(question.getQuestion().isEmpty())) {
-                        // vraag heeft een waarde:
-                        // answers = question.getAnswers();
-                        // bepaal goede antwoord
-                        Answer correctAnswer = new Answer(goodAnswerTextField.getText(), question);
-                        question.setCorrectAnswer(correctAnswer);
-
-                        // save question
-                        questionDAO.storeOne(question);
-                        // get all answers of question from database:
-                        AnswerDAO answerDAO = new AnswerDAO(dbAccess);
-                        ArrayList<Answer> answerArrayList = answerDAO.getAnswersByQuestionId(question.getQuestionId());
-                        int correctAnswerId = 0;
-                        for (Answer answer : answerArrayList) {
-                            if (correctAnswer.getAnswer().equals(answer.getAnswer())) {
-                                correctAnswerId = answer.getAnswerId();
-                            }
-                        }
-                        //check if correct question == that question -> get externalId -> pass this id to updateGoedeAntwoordId method
-                        questionDAO.updateGoedAntwoodId(correctAnswerId, question.getQuestionId());
-
-                        System.out.println(("nieuwe vraag opgeslagen"));
-                    } else {
-                        // toon waarschuwing, je moet nog een vraag invullen
-                        warningTextNoQuestion();
-                        return;
-                    }
-                } else {
-                    // toon melding: je moet nog een quiz selecteren
-                    noQuizWarning();
-                    return;
-                }
-
-            }else {
-                return;
-            }
+            checkIfQuestionFilled(question); //check als combobox waarde heeft
         } else if (titelLabel.getText().equals(labelwijzig)) {//dit is bij wijziging van een vraag
             if(question == null){
                 return;
             }
-            int id = Integer.valueOf(idQuestion.getText());
-            question.setQuestionId(id);
-            updateQuestionById(question);
-            updateAnswersFromQuestion(question);
-            System.out.println(("vraag gewijzigd"));
+            int questionId = Integer.parseInt(idQuestion.getText());
+            updateQuestion(questionId);
         }
         dbAccess.closeConnection();
         // ga terug naar dashboard
         Main.getSceneManager().showCoordinatorDashboard();
     }
 
+    private void updateQuestion(int questionId) {
+        question.setQuestionId(questionId);
+        updateQuestionById(question);
+        updateAnswersFromQuestion(question);
+        System.out.println(("vraag gewijzigd"));
+    }
+
+    private void checkIfQuestionFilled(Question question) {
+        if(question != null) {
+            if (!(quizComboBox.getValue() == null)) {
+                //als combobox waarde creer vraag
+                vullVraagBijQuizz(quizComboBox.getValue()); // vul quiz in die bij vraag hoort
+            } else {
+                // toon melding: je moet nog een quiz selecteren
+                noQuizWarning();
+                return;
+            }
+        }else {
+            return;
+        }
+    }
+
+    //vull vraag die bij quiz hoort
+    private void vullVraagBijQuizz(Quiz value) {
+
+        question.setQuiz(value);
+        // check of je vraag een waarde heeft:
+        if (!(question.getQuestion().isEmpty())) {
+            // vraag heeft een waarde:
+            // answers = question.getAnswers();
+            // bepaal goede antwoord
+
+            Answer correctAnswer = new Answer(goodAnswerTextField.getText(), question);
+            question.setCorrectAnswer(correctAnswer);
+            // save question
+            QuestionDAO questionDAO = new QuestionDAO(dbAccess);
+            questionDAO.storeOne(question);
+            // get all answers of question from database:
+            AnswerDAO answerDAO = new AnswerDAO(dbAccess);
+            ArrayList<Answer> answerArrayList = answerDAO.getAnswersByQuestionId(question.getQuestionId());
+            getCorrectAnswer(answerArrayList, correctAnswer);
+        } else {
+            // toon waarschuwing, je moet nog een vraag invullen
+            warningTextNoQuestion();
+            return;
+        }
+    }
+
+    private void getCorrectAnswer(ArrayList<Answer> answerArrayList, Answer correctAnswer) {
+        dbAccess.openConnection();
+        QuestionDAO questionDAO= new QuestionDAO(dbAccess);
+        int correctAnswerId = 0;
+        for (Answer answer : answerArrayList) {
+            if (correctAnswer.getAnswer().equals(answer.getAnswer())) {
+                correctAnswerId = answer.getAnswerId();
+            }
+        }
+        questionDAO.updateGoedAntwoodId(correctAnswerId, question.getQuestionId());
+
+        System.out.println(("nieuwe vraag opgeslagen"));
+        dbAccess.closeConnection();
+    }
+
     private void updateAnswersFromQuestion(Question question) {
         //haal aantwoorden uit vragen en zet ze in een array
-
+        AnswerDAO answerDOA = new AnswerDAO(dbAccess);
         answers = question.getAnswers();
 
-        answers.get(0).setAnswerId(Integer.valueOf(idGoodAnswer.getText()));
+        answers.get(0).setAnswerId(Integer.parseInt(idGoodAnswer.getText()));
         answers.get(1).setAnswerId(Integer.parseInt(idAnswer2.getText()));
         answers.get(2).setAnswerId(Integer.parseInt(idAnswer3.getText()));
         answers.get(3).setAnswerId(Integer.parseInt(idAnswer4.getText()));
 
 
 
-
-
         //update voor iedere vraag hun String
         for(Answer answer: answers){
-            answerDAO.updateAnswer(answer);
+            answerDOA.updateAnswer(answer);
         }
 
 
@@ -223,11 +240,7 @@ public class CreateUpdateQuestionController {
     }
 
     private boolean validateAnswerString(String rawAnswerString) {
-        if(rawAnswerString.equals("") || rawAnswerString == null){
-            return false;
-        } else{
-            return true;
-        }
+        return !rawAnswerString.equals("") && rawAnswerString != null;
     }
 
     private void noQuizWarning() {
@@ -244,7 +257,6 @@ public class CreateUpdateQuestionController {
         foutmelding.setContentText(warningText.toString());
         foutmelding.show();
         warningText.delete(0,warningText.length());
-        return ;
 
     }
 
@@ -285,7 +297,6 @@ public class CreateUpdateQuestionController {
             allAnswerFilledWarning();
             question = null;
         }
-
 
         dbAccess.closeConnection();
 
--- src/main/test/database/nosql/AnswerCouchDBDAOTest.java
+++ src/main/test/database/nosql/AnswerCouchDBDAOTest.java
@@ -1,7 +1,12 @@
 package database.nosql;
 
 import CouchDBControllers.AnswerCouchDBcontroller;
+import database.mysql.AnswerDAO;
+import database.mysql.DBAccess;
+import database.mysql.QuestionDAO;
 import model.Answer;
+import model.Question;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,6 +15,12 @@ class AnswerCouchDBDAOTest {
 
     @Test
     void getAnswer() {
+
+    }
+
+    @Test
+    void getAnswerByDocId() {
+
         //creer dbcouch object
         CouchDBaccess couchDBaccess = new CouchDBaccess();
         //connectie met couchdb
@@ -24,19 +35,27 @@ class AnswerCouchDBDAOTest {
         //creer een AntwoordcouchDBAOcontroller instantie
         AnswerCouchDBDAO answerCouchDBDAO = new AnswerCouchDBDAO(couchDBaccess);
         //haal antwoord uit couch db
-        Answer answer = answerCouchDBDAO.getAnswer("Washington");
+        Answer answerExpected = answerCouchDBDAO.getAnswerByDocId("8fe39325cf2641a08669bca4e67cade9");
 
         //sla op als expected antwoord
-        //sluit couch cd connectie
+        //sluit couch cd connectie ??
         //creer dbobject
+        DBAccess dbAccess = new DBAccess(DBAccess.getDatabaseName(), DBAccess.getMainUser(), DBAccess.getMainUserPassword());
         //connectie met dbsql
-        //creer een Vraag instantie
-        //haal vraag op die vraagd welke hoofstad van Vs
-        //haal id vraag op
-        //creer een Antwoord instantie
+        dbAccess.openConnection();
+        //creer een VraagDAO instantie
+        QuestionDAO questionDAO = new QuestionDAO(dbAccess);
+        //haal id goede antwoord in vraag op die goede aantwoord geef van hoofstad van Vs
+        int idGoedeVraag = questionDAO.getGoodAnswer(123);
+        //creer een AntwoordDAO instantie
+        AnswerDAO answerDAO = new AnswerDAO(dbAccess);
         //haal antwoord op op basis van de id vraag
+        Answer answerToBeTested = answerDAO.getOneById(idGoedeVraag);
         //sla aantwoord op als antwoord to check
         //sluit dbsql connectie
+        dbAccess.closeConnection();
         //vergelijke beide antwoorden
+
+        Assert.assertEquals(answerExpected,answerToBeTested);
     }
 }
